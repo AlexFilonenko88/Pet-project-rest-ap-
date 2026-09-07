726f1c2a01629567f4a73792f1346ce6435fcbc6
AlexFilonenko88 <af88_88@mail.ru>
2026-09-07 21:47:51 +0300

    Add endpoint /task (post); add TaskSchema

--- todo-app-backend/main.py
+++ todo-app-backend/main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 
 app = FastAPI()
 
+tasks = list[TaskSchema] = []
+
+
+class TaskSchema(BaseModel):
+    id: str
+    title: str
+    completed: str
 
 
 @app.get('/')
@@ -10,3 +18,12 @@ def read_base_page():
     return {'message': 'Hello World'}
 
 
+@app.get('/tasks')
+def read_tasks():
+    return tasks
+
+
+@app.post('/task')
+def create_task(payload):
+    new_task = TaskSchema
+    tasks.append(new_task)
