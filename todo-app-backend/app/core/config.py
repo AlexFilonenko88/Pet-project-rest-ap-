fda5c4d586c48ac53266c4c44bf2ba34f15f0b1c
AlexFilonenko88 <af88_88@mail.ru>
2026-09-15 10:33:44 +0300

    Fix connection db

--- todo-app-backend/app/core/config.py
+++ todo-app-backend/app/core/config.py
@@ -1,7 +1,7 @@
 import os
-from dotenv import load_dotenv
 from dataclasses import dataclass
 
+from dotenv import load_dotenv
 
 load_dotenv()
 
@@ -12,10 +12,14 @@ class Settings:
     cors_allowed_origin: list[str]
 
 
-def  get_settings() -> Settings:
+def get_settings() -> Settings:
+    database_url = os.getenv("DATABASE_URL")
+    if not database_url:
+        raise ValueError("DATABASE_URL is not set")
+
     return Settings(
-        DATABASE_URL = os.getenv("DATABASE_URL"),
-        cors_allowed_origin = ["http://localhost:3000"],
+        DATABASE_URL=database_url,
+        cors_allowed_origin=["http://localhost:3000"],
     )
 
 
